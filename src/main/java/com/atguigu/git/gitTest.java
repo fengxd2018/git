f8089cc6c0fdcedd601f76aedb3fb31fa23a5945
FengXD <FengXD@atguigu.com>
2020-11-27 16:16:25 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hadoop");
+        System.out.println("hot-fix");
     }
 }
